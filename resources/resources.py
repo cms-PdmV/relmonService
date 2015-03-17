2d53bbc28e7a59656ef00312f6a1d6641a99e7c2
jonasdaugalas <jonasdaugalas@gmail.com>
2015-03-17 13:57:49 +0100

    Refactor resources code duplication

--- resources/resources.py
+++ resources/resources.py
@@ -7,138 +7,11 @@ import time
 from flask import request
 from common import utils, relmon_shared
 
-# TODO: eliminate code duplication
-# # try, except, return
 
-
-class Sample(Resource):
-
-    def get(self, request_id, category, sample_list, sample_name):
+def add_default_HTTP_returns(func):
+    def decorator(*args, **kwargs):
         try:
-            relmon_request = [i for i in relmon_shared.data if
-                              i["id"] == request_id][0]
-            the_category = [i for i in relmon_request["categories"] if
-                            i["name"] == category][0]
-            the_list = the_category["lists"][sample_list]
-            return [i for i in the_list if i["name"] == sample_name][0], 200
-        except IndexError as err:
-            print(err)
-            return "Not Found", 404
-        except Exception as ex:
-            print(ex)
-            return "Internal error", 500
-
-    def put(self, request_id, category, sample_list, sample_name):
-        try:
-            with relmon_shared.data_lock:
-
-                relmon_request = [i for i in relmon_shared.data if
-                                  i["id"] == request_id][0]
-                the_category = [i for i in relmon_request["categories"] if
-                                i["name"] == category][0]
-                the_list = the_category["lists"][sample_list]
-                for sidx, sample in enumerate(the_list):
-                    if (sample["name"] == sample_name):
-                        the_list[sidx] = request.json
-                        break
-                frac_downloaded = utils.sample_fraction_by_status(
-                    relmon_request,
-                    status=["downloaded"],
-                    ignore=["NoDQMIO"])
-                if (relmon_request["status"] == "downloading" and
-                    frac_downloaded * 100 >= relmon_request["threshold"]):
-                    # then:
-                    relmon_request["status"] = "downloaded"
-                relmon_shared.write_data()
-                return "OK", 200
-        except IndexError as err:
-            print(err)
-            return "Not Found", 404
-        except Exception as ex:
-            print(ex)
-            return "Internal error", 500
-
-
-class RequestStatus(Resource):
-
-    def put(self, request_id):
-        try:
-            with relmon_shared.data_lock:
-                relmon_request = [i for i in relmon_shared.data if
-                                  i["id"] == request_id][0]
-                # TODO: check new_status for validity
-                new_status = request.json["value"]
-                relmon_request["status"] = new_status
-                relmon_shared.write_data()
-                return "OK", 200
-        except IndexError as err:
-            print(err)
-            return "Not Found", 404
-        except Exception as ex:
-            print(ex)
-            return "Internal error", 500
-
-
-class RequestLog(Resource):
-
-    def put(self, request_id):
-        try:
-            with relmon_shared.data_lock:
-                relmon_request = [i for i in relmon_shared.data if
-                                  i["id"] == request_id][0]
-                # TODO: check new_status for validity
-                new_log_state = request.json["value"]
-                relmon_request["log"] = new_log_state
-                relmon_shared.write_data()
-                return "OK", 200
-        except IndexError as err:
-            print(err)
-            return "Not Found", 404
-        except Exception as ex:
-            print(ex)
-            return "Internal error", 500
-
-
-class Request(Resource):
-
-    def get(self, request_id):
-        try:
-            return [i for i in relmon_shared.data if
-                    i["id"] == request_id][0], 200
-        except IndexError as err:
-            print(err)
-            return "Not Found", 404
-        except Exception as ex:
-            print(ex)
-            return "Internal error", 500
-
-
-class Requests(Resource):
-
-    def get(self):
-        return relmon_shared.data, 200
-
-    def post(self):
-        try:
-            args = request.json
-            new_record = {"id": int(time.time()),
-                          "name": args["name"],
-                          "status": "initial",
-                          "threshold": int(args["threshold"]),
-                          "log": False,
-                          "categories": []}
-            if (new_record["threshold"] < 0 or new_record["threshold"] > 100):
-                raise ValueError
-            for category in args["categories"]:
-                for list_idx, sample_list in category["lists"].iteritems():
-                    for sample_idx, sample in enumerate(sample_list):
-                        tmp_sample = {"name": sample, "status": "initial"}
-                        sample_list[sample_idx] = tmp_sample
-                new_record["categories"].append(category)
-            with relmon_shared.data_lock:
-                relmon_shared.data.append(new_record)
-                relmon_shared.write_data()
-            return "OK", 200
+            return func(*args, **kwargs)
         except (TypeError, ValueError) as err:
             print(err)
             return "Bad request", 400
@@ -148,6 +21,107 @@ class Requests(Resource):
         except Exception as ex:
             print(ex)
             return "Internal error", 500
+    return decorator
+
+
+class Sample(Resource):
+
+    @add_default_HTTP_returns
+    def get(self, request_id, category, sample_list, sample_name):
+        relmon_request = [i for i in relmon_shared.data if
+                          i["id"] == request_id][0]
+        the_category = [i for i in relmon_request["categories"] if
+                        i["name"] == category][0]
+        the_list = the_category["lists"][sample_list]
+        return [i for i in the_list if i["name"] == sample_name][0], 200
+
+    @add_default_HTTP_returns
+    def put(self, request_id, category, sample_list, sample_name):
+        with relmon_shared.data_lock:
+            relmon_request = [i for i in relmon_shared.data if
+                              i["id"] == request_id][0]
+            the_category = [i for i in relmon_request["categories"] if
+                            i["name"] == category][0]
+            the_list = the_category["lists"][sample_list]
+            for sidx, sample in enumerate(the_list):
+                if (sample["name"] == sample_name):
+                    the_list[sidx] = request.json
+                    break
+            frac_downloaded = utils.sample_fraction_by_status(
+                relmon_request,
+                status=["downloaded"],
+                ignore=["NoDQMIO"])
+            if (relmon_request["status"] == "downloading" and
+                frac_downloaded * 100 >= relmon_request["threshold"]):
+                # then:
+                relmon_request["status"] = "downloaded"
+            relmon_shared.write_data()
+            return "OK", 200
+
+
+class RequestStatus(Resource):
+
+    @add_default_HTTP_returns
+    def put(self, request_id):
+        with relmon_shared.data_lock:
+            relmon_request = [i for i in relmon_shared.data if
+                              i["id"] == request_id][0]
+            # TODO: check new_status for validity
+            new_status = request.json["value"]
+            relmon_request["status"] = new_status
+            relmon_shared.write_data()
+            return "OK", 200
+
+
+class RequestLog(Resource):
+
+    @add_default_HTTP_returns
+    def put(self, request_id):
+        with relmon_shared.data_lock:
+            relmon_request = [i for i in relmon_shared.data if
+                              i["id"] == request_id][0]
+            # TODO: check new_status for validity
+            new_log_state = request.json["value"]
+            relmon_request["log"] = new_log_state
+            relmon_shared.write_data()
+            return "OK", 200
+
+
+class Request(Resource):
+
+    @add_default_HTTP_returns
+    def get(self, request_id):
+        return [i for i in relmon_shared.data if
+                i["id"] == request_id][0], 200
+
+
+class Requests(Resource):
+
+    @add_default_HTTP_returns
+    def get(self):
+        return relmon_shared.data, 200
+
+    @add_default_HTTP_returns
+    def post(self):
+        args = request.json
+        new_record = {"id": int(time.time()),
+                      "name": args["name"],
+                      "status": "initial",
+                      "threshold": int(args["threshold"]),
+                      "log": False,
+                      "categories": []}
+        if (new_record["threshold"] < 0 or new_record["threshold"] > 100):
+            raise ValueError
+        for category in args["categories"]:
+            for list_idx, sample_list in category["lists"].iteritems():
+                for sample_idx, sample in enumerate(sample_list):
+                    tmp_sample = {"name": sample, "status": "initial"}
+                    sample_list[sample_idx] = tmp_sample
+            new_record["categories"].append(category)
+        with relmon_shared.data_lock:
+            relmon_shared.data.append(new_record)
+            relmon_shared.write_data()
+        return "OK", 200
 
 
 class Terminator(Resource):
